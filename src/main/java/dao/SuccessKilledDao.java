0bc94f5121a7dd922597ab0c4c36956cc0b7b1d5
developerzzp <2379759974@qq.com>
2018-10-02 11:39:36 +0800

    add git ignore file

--- src/main/java/dao/SuccessKilledDao.java
+++ src/main/java/dao/SuccessKilledDao.java
@@ -2,13 +2,17 @@ package dao;
 
 import java.util.List;
 
-public interface SuccessKilled {
+import org.apache.ibatis.annotations.Param;
+
+import dto.SuccessKilled;
+
+public interface SuccessKilledDao {
 	/**
 	 * 插入成功秒杀商品信息
 	 * @param successKilled
 	 * @return
 	 */
-	int insertSuccessKill(long seckillId,String userPhone);
+	int insertSuccessKill(@Param("seckillId") long seckillId,@Param("userPhone")String userPhone);
 	/**
 	 * 
 	 * @param seckillId
---
+++ src/test/java/dao/SuccessKilledDaoTest.java
@@ -0,0 +1,34 @@
+package dao;
+import java.util.List;
+import javax.annotation.Resource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import dto.SuccessKilled;
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉Jnit Spring 配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SuccessKilledDaoTest {
+	@Resource
+	private SuccessKilledDao successKilledDao;
+
+	@Test
+	public void testInsertSuccessKill() {
+		long seckillId = 1000;
+		String userPhone = "18487376971";
+		int n = successKilledDao.insertSuccessKill(seckillId, userPhone);
+		System.out.println(n);
+	}
+
+	@Test
+	public void testQueryByIdWithSuccessKill() {
+		long seckillId = 1000;
+		List<SuccessKilled> seckills = successKilledDao.queryByIdWithSuccessKill(seckillId);
+		for(SuccessKilled successKilled:seckills) {
+			System.out.println(successKilled);
+			
+		}
+	}
+
+}
